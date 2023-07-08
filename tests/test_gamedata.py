8eb1e7cd8e9e741b57c8995233d608915627c71e
sadru <thesadru@gmail.com>
2023-07-08 23:26:19 +0200

    Add back matrix

--- tests/test_gamedata.py
+++ tests/test_gamedata.py
@@ -10,3 +10,8 @@ async def test_download(client: arkprts.Client) -> None:
 def test_access(client: arkprts.Client) -> None:
     operator = client.gamedata.character_table["char_002_amiya"]
     assert operator.name == "Amiya"
+
+
+def calculate_trust(client: arkprts.Client) -> None:
+    # 9915 - 10069
+    assert client.gamedata.calculate_trust_level(10000) == 99
