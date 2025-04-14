b131c46541840f96e12ff6178583c4bf7ebd02bb
ashlen <thesadru@gmail.com>
2025-04-14 18:50:50 +0200

    Edit dependencies to allow for publishing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="arkprts",
-    version="0.3.14",
+    version="0.3.15",
     description="Arknights python wrapper.",
     url="https://github.com/thesadru/arkprts",
     packages=find_packages(exclude=["tests", "tests.*"]),
@@ -13,10 +13,10 @@ setup(
     package_data={"arkprts": ["py.typed"]},
     install_requires=["aiohttp", "pydantic==2.*"],
     extras_require={
-        "all": ["rsa", "pycryptodome", "UnityPy @ git+https://github.com/MooncellWiki/UnityPy@22f5d608c5c92699e83c16f04f854462f3eb2bde", "bson"],
+        "all": ["rsa", "pycryptodome", "UnityPy", "bson"],
         "rsa": ["rsa"],
         "aes": ["pycryptodome"],
-        "assets": ["UnityPy @ git+https://github.com/MooncellWiki/UnityPy@22f5d608c5c92699e83c16f04f854462f3eb2bde", "pycryptodome", "bson"],
+        "assets": ["UnityPy", "pycryptodome", "bson"],
     },
     long_description=pathlib.Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
